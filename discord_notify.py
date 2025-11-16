1e3b47623c6993906f1e71156ecdc1ec81c01ad0
Vjetdev2003 <viettrun2@gmail.com>
2025-11-17 01:51:49 +0700

    change webhook url

--- discord_notify.py
+++ discord_notify.py
@@ -1,7 +1,7 @@
 # discord_notify.py
 import requests
 
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1439527582654533642/dPuuVPdWwO7s6J1tcYJpER_1wWAx75bgNd7dxErp6dK66JUEiVhJ-M6NkQGYUzOQkHfZ"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1439689052918907092/DNBc4cwG9hIcvDpDQEjsl4Jwo-TtK2XC-S7ZcpxuVlKMKsoOauJTB_Nan-FlQxtQ7JlK"
 
 def send_discord(message: str):
     if not DISCORD_WEBHOOK_URL:
